b4b6085e1e14ed0f26c751af9aafc007c18805e3
Dusan <dusan.jovanovicno.4@gmail.com>
2023-12-04 00:59:27 +0100

    some virtual list experimental stuff

--- src/app/(name-it-better)/(survey-config)/survey/[slug]/preview/page.tsx
+++ src/app/(name-it-better)/(survey-config)/survey/[slug]/preview/page.tsx
@@ -1,52 +1,71 @@
 "use client";
-
-import { StrictModeDroppable } from "@/components/strict-mode-droppable";
 import React from "react";
-import {
-  DragDropContext,
-  Draggable,
-  DropResult,
-  Droppable,
-  ResponderProvided,
-} from "react-beautiful-dnd";
+import { useWindowVirtualizer } from "@tanstack/react-virtual";
 
 const Preview = () => {
-  function handleDragEnd(result: DropResult, provided: ResponderProvided) {
-    console.log(result);
-  }
-
+  const listRef = React.useRef<HTMLDivElement | null>(null);
+  const texts = [
+    "Vero quidem quae incidunt alias vitae quos optio tenetur consequuntur odit iure eligendi doloribus dignissimos voluptate possimus aperiam minus iusto voluptates mollitia suscipit voluptates molestias maiores unde eligendi perferendis veniam doloribus magnam enim eum harum error repellat perferendis qui a voluptate dolorem officia omnis placeat quo error minima possimus dolores sed tempora illo atque nam officia et veritatis.",
+    "Debitis earum quia nisi nemo numquam excepturi beatae optio nam nihil aperiam hic velit ipsum dolorum ducimus sapiente dolorem distinctio fuga natus aperiam dolorem id ipsam qui asperiores nihil tempore consequuntur dolor perferendis atque architecto voluptas dignissimos distinctio ducimus facere quis.",
+    "Alias ipsa vel a qui sit animi laboriosam hic nihil odit a architecto dolore unde consequatur et itaque omnis esse sunt laborum temporibus eum et suscipit.",
+    "Perspiciatis doloribus rem voluptatem omnis dolores sapiente esse rem atque quisquam voluptatem illo impedit reprehenderit vitae nisi veniam nesciunt magni quia quasi velit unde incidunt pariatur atque voluptatibus dolor nulla eius consequuntur odit sapiente quidem debitis molestias aliquam dolor.",
+  ];
+  const virtualizer = useWindowVirtualizer({
+    count: 50,
+    estimateSize: () => 50,
+    overscan: 20,
+    scrollMargin: listRef.current?.offsetTop ?? 0,
+  });
+  const items = virtualizer.getVirtualItems();
   return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <StrictModeDroppable droppableId="list">
-        {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps}>
-            <div className="list w-96">
-              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((el, index) => {
-                return (
-                  <Draggable
-                    key={el.toString()}
-                    draggableId={el.toString()}
-                    index={index}
-                  >
-                    {(provided) => (
-                      <div
-                        className="bg-red-200 h-16 mb-5"
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        Hello freind {el}
-                      </div>
-                    )}
-                  </Draggable>
-                );
-              })}
+    <div ref={listRef}>
+      <div
+        className="w-full relative"
+        style={{
+          height: `${virtualizer.getTotalSize()}px`,
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            transform: `translateY(${
+              items[0]?.start - virtualizer.options.scrollMargin
+            }px)`,
+          }}
+        >
+          {items.map((virtualRow) => (
+            <div
+              key={virtualRow.key}
+              data-index={virtualRow.index}
+              ref={virtualizer.measureElement}
+              // style={{
+              //   position: "absolute",
+              //   top: 0,
+              //   left: 0,
+              //   width: "100%",
+              //   height: `${virtualRow.size}px`,
+
+              // }}
+              className="pb-3"
+            >
+              <div style={{ padding: "10px 0" }} className="bg-red-300">
+                <div>Row {virtualRow.index}</div>
+                <div>
+                  {virtualRow.index % 2 === 0
+                    ? texts[0]
+                    : virtualRow.index % 3
+                    ? texts[1]
+                    : texts[2]}
+                </div>
+              </div>
             </div>
-            {provided.placeholder}
-          </div>
-        )}
-      </StrictModeDroppable>
-    </DragDropContext>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/app/(name-it-better)/(survey-config)/survey/[slug]/results/page.tsx
+++ src/app/(name-it-better)/(survey-config)/survey/[slug]/results/page.tsx
@@ -1,7 +1,187 @@
-import React from "react";
+"use client";
 
-const SurveyResults = () => {
-  return <div>SurveyResults</div>;
-};
+import React, { StyleHTMLAttributes, useState } from "react";
+import {
+  closestCenter,
+  DndContext,
+  DragOverlay,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { useWindowVirtualizer } from "@tanstack/react-virtual";
 
-export default SurveyResults;
+function SortableItem(props: any) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    setActivatorNodeRef,
+    isDragging,
+    transform,
+    transition,
+  } = useSortable({ id: props.id });
+
+  const style = {
+    opacity: isDragging ? "0.4" : undefined,
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <div style={style} className="p-3 bg-red-300 relative" ref={setNodeRef}>
+      Some shittt {props.id}
+      <div
+        {...attributes}
+        {...listeners}
+        ref={setActivatorNodeRef}
+        className="bg-red-500 absolute right-[-20px] w-4 top-0"
+      >
+        s
+      </div>
+    </div>
+  );
+}
+
+export default function Dsasas() {
+  const [activeId, setActiveId] = useState(null);
+  const [items, setItems] = useState([
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28",
+  ]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+  const listRef = React.useRef<HTMLDivElement | null>(null);
+  const virtualizer = useWindowVirtualizer({
+    count: items.length,
+    estimateSize: () => 50,
+    overscan: 5,
+    scrollMargin: listRef.current?.offsetTop ?? 0,
+  });
+  const itemss = virtualizer.getVirtualItems();
+
+  return (
+    <DndContext
+      id="13dsadaskk"
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+    >
+      <SortableContext items={items} strategy={verticalListSortingStrategy}>
+        {/* <div className="flex flex-col gap-2">
+          {items.map((id) => (
+            <SortableItem key={id} id={id} />
+          ))}
+        </div> */}
+        <div ref={listRef} className="py-5 px-20">
+          <div
+            className="w-full relative"
+            style={{
+              height: `${virtualizer.getTotalSize()}px`,
+            }}
+          >
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                transform: `translateY(${
+                  itemss[0]?.start - virtualizer.options.scrollMargin
+                }px)`,
+              }}
+            >
+              {itemss.map((virtualRow) => (
+                <div
+                  key={virtualRow.key}
+                  data-index={virtualRow.index}
+                  ref={virtualizer.measureElement}
+                  // style={{
+                  //   position: "absolute",
+                  //   top: 0,
+                  //   left: 0,
+                  //   width: "100%",
+                  //   height: `${virtualRow.size}px`,
+
+                  // }}
+                  className="pb-3"
+                >
+                  <SortableItem
+                    key={items[virtualRow.index]}
+                    id={items[virtualRow.index]}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </SortableContext>
+      <DragOverlay>
+        {activeId ? (
+          <div className="bg-blue-200 p-3">Some shittt {activeId}</div>
+        ) : null}
+      </DragOverlay>
+    </DndContext>
+  );
+
+  function handleDragStart(event: any) {
+    const { active } = event;
+
+    setActiveId(active.id);
+  }
+
+  function handleDragEnd(event: any) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+
+    setActiveId(null);
+  }
+}
