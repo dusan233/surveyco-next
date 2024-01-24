9390e2027f6440dc0a1f7de57f5a3eec9f8e2d50
Dusan <dusan.jovanovicno.4@gmail.com>
2024-01-25 00:40:26 +0100

    change ghost button style

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
           "border border-primary bg-secondary text-secondary-foreground hover:bg-secondary",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-secondary hover:text-primary",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
