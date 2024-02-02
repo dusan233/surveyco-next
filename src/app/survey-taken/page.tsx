9f530a92c798d16a679ab01b81925d0641921307
Dusan <dusan.jovanovicno.4@gmail.com>
2024-02-02 13:33:09 +0100

    redesign survey taken and survey thanks pages

--- src/app/survey-taken/page.tsx
+++ src/app/survey-taken/page.tsx
@@ -1,12 +1,29 @@
+import AppLogo from "@/components/layout/logo";
+import { Button } from "@/components/ui/button";
 import { CheckCircle } from "lucide-react";
+import Link from "next/link";
 
 const SurveyTakenPage = () => {
   return (
-    <div className="flex min-h-screen bg-indigo-700 flex-col items-center justify-center sm:p-24 p-3">
-      <CheckCircle className="w-12 h-12 mb-5 text-white" />
-      <h1 className="text-5xl text-center text-white font-medium max-w-3xl">
-        You have already taken this survey!
-      </h1>
+    <div className="bg-slate-800 min-h-screen flex flex-col">
+      <div className="flex-1 flex flex-col bg-slate-800 p-4 sm:p-10">
+        <div className="flex flex-1 h-full justify-center flex-col items-center gap-6">
+          <div className="justify-self-start">
+            <AppLogo theme="dark" />
+          </div>
+          <div className="flex  flex-col justify-center items-center ">
+            <CheckCircle className="w-12 h-12 mb-5 text-white" />
+            <h1 className="text-4xl sm:text-5xl  text-white text-center font-medium max-w-3xl">
+              You have already taken this survey!
+            </h1>
+          </div>
+
+          <Link href="/login">
+            <Button variant="secondary">Create survey</Button>
+          </Link>
+        </div>
+      </div>
+      <div className="flex-1 bg-secondary p-4 sm:p-10"></div>
     </div>
   );
 };
--- src/app/survey-thanks/page.tsx
+++ src/app/survey-thanks/page.tsx
@@ -11,27 +11,18 @@ const SurveyThanksPage = () => {
           <div className="justify-self-start">
             <AppLogo theme="dark" />
           </div>
-          <h1 className="text-white text-5xl">
+          <h1 className="text-white text-center text-4xl sm:text-5xl">
             Thank you for taking this survey!
           </h1>
-          <p className="text-2xl text-white">Get answers with surveys</p>
+          <p className="text-2xl text-center text-slate-300">
+            Get answers with surveys
+          </p>
           <Link href="/login">
             <Button variant="secondary">Create survey</Button>
           </Link>
         </div>
       </div>
       <div className="flex-1 bg-accent p-4 sm:p-10"></div>
-      {/* <div className="flex justify-center sm:justify-start mb-5 sm:mb-10">
-        <AppLogo theme="dark" />
-      </div>
-      <div className="flex flex-1 flex-col items-center gap-4 justify-center sm:p-24 p-3">
-        <h1 className="text-3xl sm:text-5xl text-center text-white font-medium max-w-3xl">
-          Thank you for taking this survey!
-        </h1>
-        <Link href="/login">
-          <Button variant="secondary">Create survey</Button>
-        </Link>
-      </div> */}
     </div>
   );
 };
