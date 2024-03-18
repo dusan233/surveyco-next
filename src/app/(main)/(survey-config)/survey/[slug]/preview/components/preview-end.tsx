e5f4ae4ff2ff3dd557f9bc1160bd9a5f662b0c40
Dusan <dusan.jovanovicno.4@gmail.com>
2024-03-18 15:24:46 +0100

    fix: update link path to collectors in preview-end component

--- src/app/(main)/(survey-config)/survey/[slug]/preview/components/preview-end.tsx
+++ src/app/(main)/(survey-config)/survey/[slug]/preview/components/preview-end.tsx
@@ -15,7 +15,7 @@ const PreviewEnd = ({ surveyId, restartPreview }: PreviewEndProps) => {
         <Button onClick={() => restartPreview()} size="sm">
           Preview again
         </Button>
-        <Link href={`/survey/${surveyId}/collect-responses`}>
+        <Link href={`/survey/${surveyId}/collectors`}>
           <Button size="sm">Collect responses</Button>
         </Link>
       </div>
