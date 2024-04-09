29f9f98049d0c67ea018272060e368648a06c5c8
Dusan <dusan.jovanovicno.4@gmail.com>
2024-04-09 19:00:28 +0200

    refactor: remove unused useClickAway hook

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -157,27 +157,3 @@ export const surveyCategoriesList = [
     label: "General market research",
   },
 ];
-
-export function on<T extends Window | Document | HTMLElement | EventTarget>(
-  obj: T | null,
-  ...args: Parameters<T["addEventListener"]> | [string, Function | null, ...any]
-): void {
-  if (obj && obj.addEventListener) {
-    obj.addEventListener(
-      ...(args as Parameters<HTMLElement["addEventListener"]>)
-    );
-  }
-}
-
-export function off<T extends Window | Document | HTMLElement | EventTarget>(
-  obj: T | null,
-  ...args:
-    | Parameters<T["removeEventListener"]>
-    | [string, Function | null, ...any]
-): void {
-  if (obj && obj.removeEventListener) {
-    obj.removeEventListener(
-      ...(args as Parameters<HTMLElement["removeEventListener"]>)
-    );
-  }
-}
