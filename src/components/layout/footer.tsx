59564ff047b4a465f48d2e0b1107e6952e1ed507
Dusan <dusan.jovanovicno.4@gmail.com>
2024-05-20 00:00:00 +0200

    refactor use footer html element

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="p-4 sm:p-10 text-center text-primary">
+    <footer className="p-4 sm:p-10 text-center text-primary">
       Copyright © 2024 Surveyco
-    </div>
+    </footer>
   );
 };
 
