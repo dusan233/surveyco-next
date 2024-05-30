1f35f4e32c400c8844dcf471858bf9f1dcc313bf
Dusan <dusan.jovanovicno.4@gmail.com>
2024-05-30 20:07:51 +0200

    refactor: change footer bg-color

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="p-4 sm:p-10 text-center text-primary">
+    <footer className="p-4 sm:p-10 text-center bg-slate-800 text-white">
       Copyright © 2024 Surveyco
     </footer>
   );
